2017e24b46d4a9657a85638a8634a8e4ed137ca3
Romain Beurdouche <romain.beurdouche@eurecom.fr>
2026-05-12 11:02:33 +0200

    feat(physim): add distribution in physims
    Signed-off-by: Romain Beurdouche <romain.beurdouche@eurecom.fr>

--- openair1/SIMULATION/NR_PHY/dlsim.c
+++ openair1/SIMULATION/NR_PHY/dlsim.c
@@ -1064,6 +1064,7 @@ int main(int argc, char **argv)
   time_stats_t channel_stats = {0};
   time_stats_t noise_stats = {0};
   time_stats_t pipeline_stats = {0};
+  init_sorted_list_meas(&gNB->phy_proc_tx, 4 * n_trials);
 
   for (SNR = snr0; SNR < snr1 && !stop; SNR += .2) {
 
@@ -1556,6 +1557,7 @@ int main(int argc, char **argv)
 
   } // NSR
 
+  free_sorted_list_meas(&gNB->phy_proc_tx);
   free(Sched_INFO);
 
   free_channel_desc_scm(gNB2UE);

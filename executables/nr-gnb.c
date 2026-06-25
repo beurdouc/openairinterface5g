1994251fc50526bd27d34bb455ba94f9d1b63e39
Ilhan Ghauri <ilhan.ghauri@openairinterface.org>
2026-06-25 11:53:33 +0000

    fix(rt): make capture snapshots opt-in

--- executables/nr-gnb.c
+++ executables/nr-gnb.c
@@ -72,6 +72,7 @@ static void load_l1tx_rt_deadline_config_once(void)
   g_l1tx_rt_deadline_cfg.threshold_us[2] = 1000;
   g_l1tx_rt_deadline_cfg.threshold_us[3] = 2000;
   g_l1tx_rt_deadline_cfg.capture_enable = 0;
+  g_l1tx_rt_deadline_cfg.capture_snapshot_enable = 0;
   g_l1tx_rt_deadline_cfg.capture_samples = 20000;
   snprintf(g_l1tx_rt_deadline_cfg.capture_path,
            sizeof(g_l1tx_rt_deadline_cfg.capture_path),
@@ -85,6 +86,7 @@ static void load_l1tx_rt_deadline_config_once(void)
   int threshold2_us = (int)g_l1tx_rt_deadline_cfg.threshold_us[2];
   int threshold3_us = (int)g_l1tx_rt_deadline_cfg.threshold_us[3];
   int capture_enable = g_l1tx_rt_deadline_cfg.capture_enable;
+  int capture_snapshot_enable = g_l1tx_rt_deadline_cfg.capture_snapshot_enable;
   int capture_samples = (int)g_l1tx_rt_deadline_cfg.capture_samples;
 
   paramdef_t RTDeadlineL1TXParams[] = {
@@ -96,6 +98,7 @@ static void load_l1tx_rt_deadline_config_once(void)
     {"threshold2_us", NULL, 0, .iptr = &threshold2_us, .defintval = threshold2_us, TYPE_INT, 0, NULL},
     {"threshold3_us", NULL, 0, .iptr = &threshold3_us, .defintval = threshold3_us, TYPE_INT, 0, NULL},
     {"capture_enable", NULL, 0, .iptr = &capture_enable, .defintval = capture_enable, TYPE_INT, 0, NULL},
+    {"capture_snapshot_enable", NULL, 0, .iptr = &capture_snapshot_enable, .defintval = capture_snapshot_enable, TYPE_INT, 0, NULL},
     {"capture_samples", NULL, 0, .iptr = &capture_samples, .defintval = capture_samples, TYPE_INT, 0, NULL},
   };
 
@@ -109,11 +112,12 @@ static void load_l1tx_rt_deadline_config_once(void)
   g_l1tx_rt_deadline_cfg.threshold_us[2] = threshold2_us > 0 ? (uint64_t)threshold2_us : g_l1tx_rt_deadline_cfg.threshold_us[2];
   g_l1tx_rt_deadline_cfg.threshold_us[3] = threshold3_us > 0 ? (uint64_t)threshold3_us : g_l1tx_rt_deadline_cfg.threshold_us[3];
   g_l1tx_rt_deadline_cfg.capture_enable = capture_enable;
+  g_l1tx_rt_deadline_cfg.capture_snapshot_enable = capture_snapshot_enable;
   g_l1tx_rt_deadline_cfg.capture_samples = capture_samples > 0 ? (uint64_t)capture_samples : g_l1tx_rt_deadline_cfg.capture_samples;
 
   printf("RT_DEADLINE_CONFIG_L1TX enabled=%d report_period=%lu late_threshold_us=%lu "
          "threshold0_us=%lu threshold1_us=%lu threshold2_us=%lu threshold3_us=%lu "
-         "capture_enable=%d capture_samples=%lu capture_path=%s\n",
+         "capture_enable=%d capture_snapshot_enable=%d capture_samples=%lu capture_path=%s\n",
          g_l1tx_rt_deadline_cfg.enabled,
          g_l1tx_rt_deadline_cfg.report_period,
          g_l1tx_rt_deadline_cfg.late_threshold_us,
@@ -122,6 +126,7 @@ static void load_l1tx_rt_deadline_config_once(void)
          g_l1tx_rt_deadline_cfg.threshold_us[2],
          g_l1tx_rt_deadline_cfg.threshold_us[3],
          g_l1tx_rt_deadline_cfg.capture_enable,
+         g_l1tx_rt_deadline_cfg.capture_snapshot_enable,
          g_l1tx_rt_deadline_cfg.capture_samples,
          g_l1tx_rt_deadline_cfg.capture_path);
   fflush(stdout);
